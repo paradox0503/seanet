548f4b6c69ea66e44d94aa0d84e0c3cf61e4b0b9
paradox0503 <jialinhan123@126.com>
2026-08-25 17:05:58 +0800

    更换数据集为原始数据集

--- util/experiment.py
+++ util/experiment.py
@@ -45,19 +45,19 @@ class EmbedConfig:
         self.size_query = size_query
 
 DATASET_CONFIGS = [
-    DatasetConfig("Astro00", "/data/user_jialinhan/process_data_get_record/build/data/astro-dataset.bin", 256, 400000, 10000, 100000000,0),
-    DatasetConfig("Deep1B", "/data/user_jialinhan/process_data_get_record/build/data/deep1b-dataset.bin", 96, 400000, 10000, 100000000,1),
-    DatasetConfig("F5", "/data/user_jialinhan/process_data_get_record/build/data/F5-dataset.bin", 256, 400000, 10000, 100000000,2),
-    DatasetConfig("F10", "/data/user_jialinhan/process_data_get_record/build/data/F10-dataset.bin", 256, 400000, 10000, 100000000,3),
-    DatasetConfig("origin", "/data/user_jialinhan/process_data_get_record/build/data/origin-dataset.bin", 256, 400000, 10000, 100000000,4),
-    DatasetConfig("sald", "/data/user_jialinhan/process_data_get_record/build/data/sald-dataset.bin", 128, 400000, 10000, 100000000,5),
-    DatasetConfig("seismic", "/data/user_jialinhan/process_data_get_record/build/data/seismic-dataset.bin", 256, 400000, 10000, 100000000,6)
+    DatasetConfig("Astro00", "/data/user_jialinhan/data_big/astro-dataset.bin", 256, 400000, 10000, 100000000,0),
+    DatasetConfig("Deep1B", "/data/user_jialinhan/data_big/deep1b-dataset.bin", 96, 400000, 10000, 100000000,1),
+    DatasetConfig("F5", "/data/user_jialinhan/data_big/F5-dataset.bin", 256, 400000, 10000, 100000000,2),
+    # DatasetConfig("F10", "/data/user_jialinhan/data_big/F10-dataset.bin", 256, 400000, 10000, 100000000,3),
+    DatasetConfig("origin", "/data/user_jialinhan/data_big/origin-dataset.bin", 256, 400000, 10000, 100000000,4),
+    DatasetConfig("sald", "/data/user_jialinhan/data_big/sald-dataset.bin", 128, 400000, 10000, 100000000,5),
+    DatasetConfig("seismic", "/data/user_jialinhan/data_big/seismic-dataset.bin", 256, 400000, 10000, 100000000,6)
 ]
 embed_CONFIGS = [    #   database path                                               query path
     EmbedConfig("astro", "data_big/astro-dataset.bin",    "data_big/astro-query.bin",256,100),
     EmbedConfig("deep1b", "data_big/deep1b-dataset.bin",    "data_big/deep1b-query.bin",96,1000),
     EmbedConfig("F5", "data_big/F5-dataset.bin",    "data_big/F5-query.bin",256,1000),
-    EmbedConfig("F10", "data_big/F10-dataset.bin",    "data_big/F10-query.bin",256,1000),
+    # EmbedConfig("F10", "data_big/F10-dataset.bin",    "data_big/F10-query.bin",256,1000),
     EmbedConfig("origin", "data_big/origin-dataset.bin",    "data_big/origin-query.bin",256,1000),
     EmbedConfig("sald", "data_big/sald-dataset.bin",    "data_big/sald-query.bin",128,1000),
     EmbedConfig("seismic", "data_big/seismic-dataset.bin",    "data_big/seismic-query.bin",256,1000),
